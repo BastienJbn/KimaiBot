26cfc6ff6a09c034b771af0c7c5388a7eff8bf14
Bastien JUBINEAU <bastien.jubineau@diasys-technologies.com>
2024-07-22 19:42:44 +0200

    Fix bug when running KimaiBotService during installation Cleanup KimaiBotCmdLine main function

--- KimaiBotConsoleClient/Program.cs
+++ KimaiBotConsoleClient/Program.cs
@@ -2,55 +2,17 @@
 
 class Program
 {
-    // Main function when debugging
-    #if DEBUG
-    static void Main()
-    {
-        PipeClient client = new();
-        Parser parser = new(client);
-
-        if (!client.Connect())
-        {
-            Console.WriteLine("Failed to connect to the server!");
-            return;
-        }
-
-        Console.WriteLine("Start ! Type 'exit' to quit.");
-
-        while (true)
-        {
-            // Wait for user input
-            Console.Write(">");
-            var command = Console.ReadLine();
-
-            if(command == null || command == string.Empty)
-                continue;
-
-            if (command == "exit")
-                break;
-
-            // Parse Command and process it
-            string result = parser.HandleCommand(command);
-
-            // Display result to user
-            Console.WriteLine(result);
-        }
-
-        client.Disconnect();
-    }
-    #else
-
     // Main function when running the application
     static void Main(string[] args)
     {
         PipeClient client = new();
         Parser parser = new(client);
 
-        //if(args.Length == 0)
-        //{
-        //    Console.WriteLine("No command provided.");
-        //    Environment.Exit(1);
-        //}
+        if(args.Length == 0)
+        {
+            Console.WriteLine("No command provided.");
+            Environment.Exit(1);
+        }
 
         if (!client.Connect())
         {
@@ -58,8 +20,7 @@ class Program
             Environment.Exit(1);
         }
 
-        //string command = string.Join(" ", args);
-        string command = "login username password";
+        string command = string.Join(" ", args);
 
         // Parse Command and process it
         string result = parser.HandleCommand(command);
@@ -71,5 +32,4 @@ class Program
 
         Environment.Exit(0);
     }
-    #endif
 }
--- KimaiBotService/Program.cs
+++ KimaiBotService/Program.cs
@@ -1,38 +1,29 @@
 using KimaiBotService;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
+using System.Reflection;
 
 using CliWrap;
 
-const string ServiceName = "KimaiAutoEntry Service";
-
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddWindowsService(options =>
-{
-    options.ServiceName = ServiceName;
-});
-
-if (OperatingSystem.IsWindows())
-{
-    LoggerProviderOptions.RegisterProviderOptions<
-        EventLogSettings, EventLogLoggerProvider>(builder.Services);
-}
-
-builder.Services.AddSingleton<KimaiBot>();
-builder.Services.AddHostedService<WindowsBackgroundService>();
+const string ServiceName = "KimaiBot";
 
 if (args is { Length: 1 })
 {
     try
     {
         string executablePath =
-            Path.Combine(AppContext.BaseDirectory, "KimaiAutoEntry.exe");
+            Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.exe");
 
         if (args[0] is "/Install")
         {
-            await Cli.Wrap("sc.exe")
+            var result = await Cli.Wrap("sc.exe")
                 .WithArguments(["create", ServiceName, $"binPath={executablePath}", "start=auto"])
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteAsync();
+
+            result = await Cli.Wrap("sc.exe")
                 .WithArguments(["start", ServiceName])
+                .WithValidation(CommandResultValidation.None)
                 .ExecuteAsync();
         }
         else if (args[0] is "/Uninstall")
@@ -53,6 +44,21 @@ if (args is { Length: 1 })
 
     return;
 }
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = ServiceName;
+});
+
+if (OperatingSystem.IsWindows())
+{
+    LoggerProviderOptions.RegisterProviderOptions<
+        EventLogSettings, EventLogLoggerProvider>(builder.Services);
+}
+
+builder.Services.AddSingleton<KimaiBot>();
+builder.Services.AddHostedService<WindowsBackgroundService>();
 
 IHost host = builder.Build();
 host.Run();
