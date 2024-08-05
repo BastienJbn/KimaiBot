acf86947858db3e3d2357998a7458229607a088d
BastienJbn <b.jubineau@gmail.com>
2024-08-05 23:56:20 +0200

    Ajout de gestion de l'authentification et du minuteur
    - Ajout des variables `username`, `password` et `isAuthenticated` pour l'authentification.
    - Ajout de `triggerTime` (10h par défaut) et `timerInterval` (1 min par défaut).
    - Initialisation du minuteur dans `Start` avec un événement `Elapsed`.
    - Ajout des méthodes `StartTimer` et `StartTimerByInterval`.
    - Modification de `login` pour utiliser les nouvelles variables et démarrer le minuteur.
    - Modification de `logout` pour réinitialiser les variables et arrêter le minuteur.
    - Ajout de `PeriodicTask` pour gérer les tâches périodiques et réessayer l'authentification en cas d'échec.

--- KimaiBotService/KimaiBot.cs
+++ KimaiBotService/KimaiBot.cs
@@ -14,9 +14,21 @@ public sealed class KimaiBot(ILogger<KimaiBot> logger)
     private readonly PipeServer server = new();
     private readonly ILogger<KimaiBot> logger = logger;
 
+    private string? username = null;
+    private string? password = null;
+    private bool isAuthenticated = false;
+
+    DateTime? triggerTime = null;
+    TimeSpan timerInterval = new(0, 1, 0); // 1 minute by default
+
     public async Task Start(CancellationToken token)
     {
         logger.LogInformation("Starting KimaiBot service.");
+
+        // Init timer
+        timer.Elapsed += PeriodicTask;
+        timer.AutoReset = true;
+
         await Task.WhenAll(CommandHandler(token), server.Start(token));
     }
 
@@ -36,6 +48,34 @@ public sealed class KimaiBot(ILogger<KimaiBot> logger)
         }
     }
 
+    // Start timer to elapsed at the defined time
+    private void StartTimer() {
+        if (triggerTime == null)
+        {
+            logger.LogWarning("Heure de déclenchement non définie. Tous les jours à 10h par défaut.");
+            triggerTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 10, 0, 0);
+        }
+
+        // Calculate time until trigger
+        TimeSpan timeUntilTrigger = triggerTime.Value - DateTime.Now;
+
+        // If time is negative, add a day
+        if(timeUntilTrigger.TotalMilliseconds < 0)
+        {
+            timeUntilTrigger = timeUntilTrigger.Add(new TimeSpan(1, 0, 0, 0));
+        }
+
+        // Start timer
+        timer.Interval = timeUntilTrigger.TotalMilliseconds;
+        timer.Start();
+    }
+
+    private void StartTimerByInterval(TimeSpan interval)
+    {
+        timer.Interval = interval.TotalMilliseconds;
+        timer.Start();
+    }
+
     private string HandleCommand(string request)
     {
         string[] args = request.Split(' ');
@@ -49,34 +89,40 @@ public sealed class KimaiBot(ILogger<KimaiBot> logger)
         switch (args[0])
         {
             case "login":
-                string username = args[1];
-                string password = args[2];
+                username = args[1];
+                password = args[2];
 
                 logger.LogInformation("Tentative d'authentification...");
 
-                if (httpClient.Authenticate(username, password))
+                if(httpClient.Authenticate(username, password))
                 {
-                    // Add entry immediately
-                    httpClient.AddEntryComboRnD();
-
-                    // Start timer. Trigger each day at 10am
-                    timer.Elapsed += (sender, e) => httpClient.AddEntryComboRnD();
-                    timer.Interval = 1000 * 60 * 60 * 24;
-                    timer.Start();
-
+                    isAuthenticated = true;
                     logger.LogInformation("Authentification réussie.");
+
+                    // Set timer to trigger at configured time (triggerTime)
+                    StartTimer();
                     return "Successfully logged in.";
                 }
                 else
                 {
+                    isAuthenticated = false;
                     logger.LogError("Authentification échouée.");
+
+                    // Set timer to trigger every minute
+                    StartTimerByInterval(new TimeSpan(0, 1, 0));
+
                     return "Failed to log in.";
                 }
 
             case "logout":
+                username = null;
+                password = null;
+                isAuthenticated = false;
+
                 httpClient.Logout();
                 timer.Stop();
                 logger.LogInformation("Utilisateur déconnecté.");
+
                 return "Successfully logged out.";
 
             case "addEntry":
@@ -94,6 +140,57 @@ public sealed class KimaiBot(ILogger<KimaiBot> logger)
             default:
                 logger.LogWarning("Commande reçue invalide.");
                 return "Invalid command.";
+        }
+    }
+
+    private void PeriodicTask(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        if(username != null && password != null)
+        {
+            if(!isAuthenticated)
+            {
+                // Try to authenticate
+                if(httpClient.Authenticate(username, password))
+                {
+                    isAuthenticated = true;
+                    logger.LogInformation("Authentification réussie.");
+
+                    // Add entry
+                    if(httpClient.AddEntryComboRnD())
+                    {
+                        logger.LogInformation("Entrée ajoutée.");
+                    }
+                    else
+                    {
+                        logger.LogError("Échec de l'ajout de l'entrée.");
+                    }
+
+                    // Set timer to trigger at triggerTime
+                    StartTimer();
+                }
+                else
+                {
+                    logger.LogError("Authentification échouée.");
+                }
+            }
+            else
+            {
+                // Add entry
+                if(httpClient.AddEntryComboRnD())
+                {
+                    logger.LogInformation("Entrée ajoutée.");
+                }
+                else
+                {
+                    logger.LogError("Échec de l'ajout de l'entrée.");
+
+                    // HttpClient failed to add entry, try to re-authenticate
+                    isAuthenticated = false;
+
+                    // Set timer to trigger every minute
+                    StartTimerByInterval(new TimeSpan(0, 1, 0));
+                }
+            }
         }
     }
 }
